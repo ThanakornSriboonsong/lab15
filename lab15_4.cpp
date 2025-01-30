d8df266d643eabb699bfb4a8555a21a1187bc71b
Pleum <plemplemplemplem5@gmail.com>
2025-01-30 16:45:47 +0700

    done

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -18,4 +18,27 @@ int main(){
 	return 0;
 }
 
+
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int num[4] = { 50 ,100 , 500 , 1000 } ; 
+	for(int i = 3 ; i > 0 ; i--){
+		int j = rand()%(i+1) ;
+		swap(num[i],num[j]) ;
+
+		a = num[0] ;
+		b = num[1] ;
+		c = num[2] ;
+		d = num[3] ;
+
+	}
+
+
+
+
+
+
+
+
+}
+
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -19,4 +19,25 @@ int main(){
 }
 
 //Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+//Write definition of shuffle() using pointer after this line.
+void shuffle(int* a,int* b,int* c,int* d){
+	int num[4] = { 50 ,100 , 500 , 1000 } ; 
+	for(int i = 3 ; i > 0 ; i--){
+		int j = rand()%(i+1) ;
+		swap(num[i],num[j]) ;
+
+		*a = num[0] ;
+		*b = num[1] ;
+		*c = num[2] ;
+		*d = num[3] ;
+
+	}
+
+
+
+
+
+
+
+
+} 
